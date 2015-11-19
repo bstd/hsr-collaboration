4953bf2cd8091adb6ce7f3c7d150021aefc99991
bstadler <bstadler@namics.com>
2015-11-19 15:11:05 +0100

    add draft for test: server api user

--- server/api/user/user.spec.js
+++ server/api/user/user.spec.js
@@ -1,9 +1,81 @@
 ﻿'use strict';
 
 var should = require('should');
-//var app = require('../../app');
+var app = require('../../app');
 var request = require('supertest');
 var server = request.agent('http://localhost:9000');
+var User = require('./user.model');
+
+
+describe('requesting routes', function() {
+  beforeEach(function(done) {
+    // Clear users before testing
+    User.remove().exec().then(function() {
+      done();
+    });
+  });
+
+  after(function(done) {
+    // Clear users after testing
+    User.remove().exec().then(function() {
+      done();
+    });
+  });
+
+  // TODO use sequence as in index
+  /*
+  get('/', auth.hasRole('admin')
+  delete('/:id', auth.hasRole('admin')
+  get('/me', auth.isAuthenticated()
+  put('/:id/password', auth.isAuthenticated()
+  get('/:id', auth.hasRole('admin')
+  post('/'
+  */
+
+
+  it('GET /api/users should return 401 because restricted', function(done) {
+    request(app)
+    .get('/api/users')
+    .expect(401)
+    .end(function(err, res) {
+      if (err) return done(err);
+      done();
+    });
+  });
+
+  it('GET /api/users/:id should return 401 because restricted', function(done) {
+    request(app)
+    .get('/api/users/123')
+    .expect(401)
+    .end(function(err, res) {
+      if (err) return done(err);
+      done();
+    });
+  });
+
+  it('POST empty /api/users should return 422', function(done) {
+    request(app)
+    .post('/api/users')
+    .expect(422)
+    .expect('Content-Type', /json/)
+    .end(function(err, res) {
+      if (err) return done(err);
+      done();
+    });
+  });
+
+  it('POST {email: a@b.c, password: 123} /api/users should return 200', function(done) {
+    request(app)
+    .post('/api/users')
+    .send({ email: 'a@b.c', password: '123'})
+    .expect(200)
+    .expect('Content-Type', /json/)
+    .end(function(err, res) {
+      if (err) return done(err);
+      done();
+    });
+  });
+});
 /*
 describe('authenticated as admin GET /api/users', function() {
 
@@ -18,7 +90,6 @@ describe('authenticated as admin GET /api/users', function() {
       .expect('Content-Type', /json/)
       .end(function(err, res) {
         if (err) return done(err);
-        res.body.should.be.instanceof(Array);
         done();
       });
   });
