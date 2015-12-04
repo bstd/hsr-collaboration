0df94ecb3afd7aea3477c9c7f9a6d61f79626ad3
wickli <jol.wic@gmail.com>
2015-12-04 16:09:55 +0100

    product image client side

--- client/app/admin/products/create.controller.js
+++ client/app/admin/products/create.controller.js
@@ -34,4 +34,26 @@ angular.module('brewApp')
           });
       }
     };
+    $scope.uploadFiles = function(file, errFiles) {
+      $scope.f = file;
+      $scope.errFile = errFiles && errFiles[0];
+      if (file) {
+        file.upload = Upload.upload({
+          url: 'api/products/upload',
+          data: {file: file}
+        });
+
+        file.upload.then(function (response) {
+          $timeout(function () {
+            file.result = response.data;
+          });
+        }, function (response) {
+          if (response.status > 0)
+            $scope.errorMsg = response.status + ': ' + response.data;
+        }, function (evt) {
+          file.progress = Math.min(100, parseInt(100.0 *
+            evt.loaded / evt.total));
+        });
+      }
+    }
   }]);
